91842f6d4551aeb659d16e804e1f2f6677a195e7
OverlordsIII <shrihunreddy@outlook.com>
2022-10-17 13:05:31 -0700

    Do more config stuff

--- src/main/java/me/elephantsuite/Main.java
+++ src/main/java/me/elephantsuite/Main.java
@@ -16,10 +16,10 @@ public class Main {
 
     public static final Logger LOGGER = LogManager.getLogger("ElephantDiscordBot");
 
-    //TODO include in gitignore to stop token getting leaked
-    public static final PropertiesHandler TOKEN = PropertiesHandler
+    public static final PropertiesHandler PRIVATE_CONFIG = PropertiesHandler
             .builder()
             .addConfigOption("token", "")
+            .addConfigOption("elephantBackendDomain", "")
             .build();
 
     public static final PropertiesHandler CONFIG = PropertiesHandler
@@ -28,25 +28,49 @@ public class Main {
             .addConfigOption("activityType", Activity.ActivityType.PLAYING.toString())
             .addConfigOption("activityText", "Helping People Memorize")
             .addConfigOption("createdCommands", "false")
+            .addConfigOption("developmentMode", "true")
             .build();
 
+    //TODO Add JSON User configs
     public static final Map<String, PropertiesHandler> SERVER_CONFIG_LISTS = new HashMap<>();
 
     public static JDA JDA;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         JDA = JDABuilder
-                .createDefault(TOKEN.getConfigOption("token"))
+                .createDefault(PRIVATE_CONFIG.getConfigOption("token"))
                 .setEventManager(new AnnotatedEventManager())
                 .setActivity(Activity.of(CONFIG.getConfigOption("activityType", Activity.ActivityType::valueOf), CONFIG.getConfigOption("activityText")))
                 .setStatus(CONFIG.getConfigOption("statusType", OnlineStatus::valueOf))
                 .build();
 
-        if (!CONFIG.getConfigOption("createdCommands", Boolean::valueOf)) {
+        if (CONFIG.getConfigOption("developmentMode", Boolean::valueOf)) {
+            JDA.getGuilds().forEach(g -> {
+                //TODO Add slash commands to register for guilds only (much faster)
+                //see https://github.com/OverlordsIII/QuotebookBot/blob/master/src/main/java/io/github/overlordsiii/Main.java#L68
+            });
+        }
+
+        if (!CONFIG.getConfigOption("createdCommands", Boolean::valueOf) && !CONFIG.getConfigOption("developmentMode", Boolean::valueOf)) {
             //TODO Add slash commands to register
             //see https://github.com/OverlordsIII/QuotebookBot/blob/master/src/main/java/io/github/overlordsiii/Main.java#L68
             CONFIG.setConfigOption("createdCommands", "true");
             CONFIG.reload();
         }
+
+        JDA.awaitReady().getGuilds().forEach(guild -> {
+            if (!SERVER_CONFIG_LISTS.containsKey(guild.getId())) {
+
+                SERVER_CONFIG_LISTS.put(guild.getId(), PropertiesHandler
+                        .builder()
+                        .serverConfig()
+                        .setFileName(guild.getId() + ".properties")
+                        //TODO Add guild config values when needed
+                        //for sanity
+                        .addConfigOption("name", guild.getName())
+                        .build()
+                );
+            }
+        });
     }
 }
